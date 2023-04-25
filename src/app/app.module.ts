6f09ffa753fca10ee33c8b7b2aacc117be920994
Ivo Peev <igpeev@gmail.com>
2023-04-25 21:02:07 +0300

    some CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppCardComponent } from './app-card/app-card.component';
 
+
 @NgModule({
     declarations: [
         AppComponent,
